e1a56653ed98d11a356660d3451fb68174264fed
KutovoyIlya <ilya.kutovoy00@mail.ru>
2025-09-28 22:35:42 +0300

    Lab 1 Finally
    All tests via VS are green and good.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Blue blue = new Blue();
+            Console.WriteLine(blue.Task1(-1, -2));
+            Console.WriteLine(blue.Task2(2));
+            Console.WriteLine(blue.Task3(2, 4));
+            Console.WriteLine(blue.Task4(-2.5, 1.89, 3.0));
+            Console.WriteLine(blue.Task5(0.5));
+            Console.WriteLine(blue.Task6(2.5, 1.89));
+            Console.WriteLine(blue.Task7(2, -3));
+        }
+    }
+}
