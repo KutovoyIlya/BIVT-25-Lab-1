3bd60667fca09fac75ca7d90828b4b190d3711c8
KutovoyIlya <ilya.kutovoy00@mail.ru>
2025-10-01 21:17:43 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,13 +5,13 @@
         public static void Main()
         {
             Blue blue = new Blue();
-            Console.WriteLine(blue.Task1(1, 2));
+            Console.WriteLine(blue.Task1(-1, -2));
             Console.WriteLine(blue.Task2(2));
-            Console.WriteLine(blue.Task3(2, 1));
-            //Console.WriteLine(white.Task4(-2.5, 1.89));
-            //Console.WriteLine(white.Task5(0.5));
-            //Console.WriteLine(white.Task6(2.5, 1.89, 3));
-            //Console.WriteLine(blue.Task7(2));
+            Console.WriteLine(blue.Task3(2, 4));
+            Console.WriteLine(blue.Task4(-2.5, 1.89, 3.0));
+            Console.WriteLine(blue.Task5(0.5));
+            Console.WriteLine(blue.Task6(2.5, 1.89));
+            Console.WriteLine(blue.Task7(2, -3));
         }
     }
 }
